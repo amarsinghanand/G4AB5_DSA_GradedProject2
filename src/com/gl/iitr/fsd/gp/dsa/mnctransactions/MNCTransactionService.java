c368f40a9d8b68cca5049062218d63cf0ff6699f
AMAR <amar.singh@openbravo.com>
2022-11-14 13:40:45 +0530

    formatted the binary search tree skewed for the mnc transactions source

--- src/com/gl/iitr/fsd/gp/dsa/mnctransactions/MNCTransactionService.java
+++ src/com/gl/iitr/fsd/gp/dsa/mnctransactions/MNCTransactionService.java
@@ -4,16 +4,16 @@ public class MNCTransactionService {
 
 	public static void main(String[] args) {
 		SkewedBinarySearchTree tree = new SkewedBinarySearchTree();
-	     tree.node = new Node(50);
-	     tree.node.left = new Node(30);
-	     tree.node.right = new Node(60);
-	     tree.node.left.left = new Node(10);
-	     tree.node.right.left= new Node(55);
-	    
-	     int order = 0; 
-	     tree.createSkewedTree(tree.node, order);
-	     System.out.println("The converted binary search tree into a skewed tree is: ");
-	     tree.traverseRightSkewed(tree.headNode);
+		tree.node = new Node(50);
+		tree.node.left = new Node(30);
+		tree.node.right = new Node(60);
+		tree.node.left.left = new Node(10);
+		tree.node.right.left = new Node(55);
+
+		int order = 0;
+		tree.createSkewedTree(tree.node, order);
+		System.out.println("The converted binary search tree into a skewed tree is: ");
+		tree.traverseRightSkewed(tree.headNode);
 
 	}
 
